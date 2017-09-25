4f437df8e7314446a5e27dd85f79e28828bbb51b
root <stever@syntithenai.com>
2017-09-25 02:14:19 +0000

    good to go

--- README.rst
+++ README.rst
@@ -1,12 +1,14 @@
-Maths skill for Snips
+HDMI Monitor control skill for Snips
 ======================================
 
 |MIT License|
 
-snips-skill-maths
+snips-skill-monitorcontrol
 
 Installation
 ------------
+
+The fastest way to get going is to use the prebuilt assistant and Snipsfile at  https://github.com/syntithenai/snips_skill_maths
 
 Usage
 -----
@@ -15,8 +17,37 @@ Snips Skills Manager
 
 It is recommended that you use this skill with the `Snips Skills Manager <https://github.com/snipsco/snipsskills>`_. Simply add the following section to your `Snipsfile <https://github.com/snipsco/snipsskills/wiki/The-Snipsfile>`_:
 
-Standalone usage
-^^^^^^^^^^^^^^^^
+package_name: monitorcontrolskill
+class_name: MonitorControlSkill
+requires_tts: True
+intents:
+  - intent: MonitorSelectBrightness
+    action: |
+      {%
+      skill.monitor_aselect_brightness(intent.monitorBrightness)
+      %}
+  - intent: MonitorSelectVolume
+    action: |
+      {%
+      skill.monitor_bselect_volume(intent.monitorVolume)
+      %}
+  - intent: MonitorOn
+    action: |
+      {%
+      skill.monitor_power_on()
+      %}
+  - intent: MonitorOff
+    action: |
+      {%
+      skill.monitor_power_off()
+      %}
+  - intent: MonitorSelectInput
+    action: |
+      {%
+      skill.monitor_select_input(intent.monitorInputNumber)
+      %}
+
+
 
 Contributing
 ------------
--- monitorcontrolskill/monitorcontrolskill.py
+++ monitorcontrolskill/monitorcontrolskill.py
@@ -16,13 +16,47 @@ class MonitorControlSkill:
         self.tts_service = tts_service
         
     def monitor_power_on(self):
-        output = subprocess.check_output('ddcutil setvcp D6 1',shell = True)
-        
+        try:
+            output = subprocess.check_output('ddcutil setvcp D6 1',shell = True)
+        except:
+            pass
     def monitor_power_off(self):
-        output = subprocess.check_output('ddcutil setvcp D6 4',shell = True)
+        try:
+            output = subprocess.check_output('ddcutil setvcp D6 4',shell = True)
+        except:
+            pass
 
     def monitor_select_input(self,monitorInputNumber):
-        output = subprocess.check_output('ddcutil setvcp 60 {}'.format(monitorInputNumber),shell = True)
+        try:
+            if monitorInputNumber < 0 or monitorInputNumber > 2:
+                if self.tts_service is not None:
+                    self.tts_service.speak('Input number must be 0,1 or 2')
+            else:
+                output = subprocess.check_output('ddcutil setvcp 60 {}'.format(int(monitorInputNumber)),shell = True)
+        except:
+            pass
 
-        #if self.tts_service:
-            #self.tts_service.speak(response)
+    def monitor_select_brightness(self,monitorBrightness):
+        try:
+            if monitorBrightness < 0 or monitorBrightness > 100:
+                if self.tts_service is not None:
+                    self.tts_service.speak('Brightness must be between 0 and 100')
+            else:
+                output = subprocess.check_output('ddcutil setvcp 10 {}'.format(int(monitorBrightness)),shell = True)
+                print(output)
+                if self.tts_service is not None:
+                    self.tts_service.speak('Set brightness to {}'.format(int(monitorBrightness)))
+        except:
+            pass
+      
+    def monitor_select_volume(self,monitorVolume):
+        try:
+            if monitorVolume < 0 or monitorVolume > 100:
+                if self.tts_service is not None:
+                    self.tts_service.speak('Volume must be between 0 and 100')
+            else:
+                output = subprocess.check_output('ddcutil setvcp 62 {}'.format(int(monitorVolume)),shell = True)
+                if self.tts_service is not None:
+                    self.tts_service.speak('Set volume to {}'.format(int(monitorVolume)))
+        except:
+            pass
